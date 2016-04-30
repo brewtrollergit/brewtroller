4c057850f6b216f3ebffd1acf63caf79937756e1
jvetter <jvetter@gmail.com>
2016-04-29 20:44:44 -0400

    Fixed UI bug that causes getHexValue menu to hang during some wrap around conditions

--- src/UI.cpp
+++ src/UI.cpp
@@ -1718,7 +1718,6 @@ unsigned long getHexValue(const char sTitle[], unsigned long defValue) {
             }
             else {
                 cursorPos = encValue;
-                multiplier = ulpow(16, (digits - cursorPos - 1));
                 for (byte i = valuePos - 1; i < valuePos - 1 + digits; i++) {
                     LCD.writeCustChar(2, i, 0);
                 }
@@ -1729,6 +1728,7 @@ unsigned long getHexValue(const char sTitle[], unsigned long defValue) {
                     LCD.print_P(3, 11, UIStrings::Generic::LESS_SYM);
                 }
                 else {
+                    multiplier = ulpow(16, (digits - cursorPos - 1));
                     if (cursorPos < digits) {
                         LCD.writeCustChar(2, valuePos + cursorPos - 1, 1);
                     }
